cf5644b1a4ce765ab4291da32c365b91bc363945
PanduDLaksono <54766392+PanduDLaksono@users.noreply.github.com>
2020-12-16 19:12:24 +0700

    up laporan recycle view & retrofit

--- src/12_Retrofit/app/src/main/java/org/polinema/retrofit/models/Repo.java
+++ src/12_Retrofit/app/src/main/java/org/polinema/retrofit/models/Repo.java
@@ -4,16 +4,15 @@ import com.google.gson.annotations.SerializedName;
 
 public class Repo {
     @SerializedName("name")
-
     private String name;
+
     @SerializedName("full_name")
-
     private String fullName;
+
     @SerializedName("html_url")
-
     private String htmlUrl;
-    @SerializedName("description")
 
+    @SerializedName("description")
     private String description;
 
     public String getName() {
